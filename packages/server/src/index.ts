37e2fb63d876b84d6f90d6321e67f7e1694d2262
Austin Shalit <austinshalit@gmail.com>
2020-07-05 22:02:22 -0700

    Switch to Koa from Express

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,10 +1,13 @@
-import { ApolloServer, PubSub } from "apollo-server";
+import { ApolloServer, PubSub } from "apollo-server-koa";
+import * as Koa from "koa";
 import { importSchema } from "graphql-import";
 import { allResolvers } from "./resolvers";
 import DbService from "./db";
 
 const db = new DbService();
 const pubsub = new PubSub();
+
+const app = new Koa();
 
 const server = new ApolloServer({
   typeDefs: importSchema("schemas/schema.graphql"),
@@ -17,9 +20,19 @@ const server = new ApolloServer({
     db,
     pubsub
   }),
-  resolvers: allResolvers
+  resolvers: allResolvers,
+  uploads: {
+    // Limits here should be stricter than config for surrounding
+    // infrastructure such as Nginx so errors can be handled elegantly by
+    // graphql-upload:
+    // https://github.com/jaydenseric/graphql-upload#type-processrequestoptions
+    maxFileSize: 10_000_000_000, // 10 GB
+    maxFiles: 20
+  }
 });
 
-server.listen().then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`);
+server.applyMiddleware({ app });
+
+app.listen(3001, () => {
+  console.info(`🚀 Server ready at  http://localhost:3001`);
 });
