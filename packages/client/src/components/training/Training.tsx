829594c4a31b71b42d374143fd84aa5785e07349
Brad Miller <brad@bradhouse.com>
2020-07-09 11:53:46 -0400

    Accidentally put the reset button inside the acordion.

--- packages/client/src/components/training/Training.tsx
+++ packages/client/src/components/training/Training.tsx
@@ -1,5 +1,16 @@
 import React from "react";
-import { Accordion, AccordionDetails, AccordionSummary, Button, Divider, Paper, TextField, Toolbar, Tooltip, Typography } from "@material-ui/core";
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Button,
+  Divider,
+  Paper,
+  TextField,
+  Toolbar,
+  Tooltip,
+  Typography
+} from "@material-ui/core";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import HelpOutlineIcon from "@material-ui/icons/HelpOutline";
 import Grid from "@material-ui/core/Grid";
@@ -13,79 +24,79 @@ export default function Training() {
         </Typography>
       </Toolbar>
       <Button variant="contained">Train Model</Button>
-      <Divider />
+      <Divider/>
       <Accordion>
-        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+        <AccordionSummary expandIcon={<ExpandMoreIcon/>}>
           <Typography>Advanced</Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Grid container spacing={2}>
 
             <Grid item xs={10}>
-            <TextField
-              variant="outlined"
-              id="standard-number"
-              label="Epochs"
-              type="number"
-              InputLabelProps={{
-                shrink: true
-              }}
-            />
+              <TextField
+                variant="outlined"
+                id="standard-number"
+                label="Epochs"
+                type="number"
+                InputLabelProps={{
+                  shrink: true
+                }}
+              />
             </Grid>
             <Grid item xs={2}>
-            <Tooltip title="The number of epochs.">
-              <HelpOutlineIcon />
-            </Tooltip>
+              <Tooltip title="The number of epochs.">
+                <HelpOutlineIcon/>
+              </Tooltip>
             </Grid>
 
             <Grid item xs={10}>
-            <TextField
-              variant="outlined"
-              id="standard-number"
-              label="Batch size"
-              type="number"
-              InputLabelProps={{
-                shrink: true
-              }}
-            />
+              <TextField
+                variant="outlined"
+                id="standard-number"
+                label="Batch size"
+                type="number"
+                InputLabelProps={{
+                  shrink: true
+                }}
+              />
             </Grid>
             <Grid item xs={2}>
-            <Tooltip title="The batch size.">
-              <HelpOutlineIcon />
-            </Tooltip>
+              <Tooltip title="The batch size.">
+                <HelpOutlineIcon/>
+              </Tooltip>
             </Grid>
 
             <Grid item xs={10}>
-            <TextField
-              variant="outlined"
-              id="standard-number"
-              label="Learning rate"
-              type="number"
-              InputLabelProps={{
-                shrink: true
-              }}
-            />
+              <TextField
+                variant="outlined"
+                id="standard-number"
+                label="Learning rate"
+                type="number"
+                InputLabelProps={{
+                  shrink: true
+                }}
+              />
             </Grid>
             <Grid item xs={2}>
-            <Tooltip title="The learning rate.">
-              <HelpOutlineIcon />
-            </Tooltip>
-            </Grid>
-            <Grid item xs={12}>
-            <Divider />
-            </Grid>
-            <Grid item xs={12}>
-            <Button>Reset defaults</Button>
-            </Grid>
-            <Grid item xs={12}>
-            <Divider />
-            </Grid>
-            <Grid item xs={12}>
-            <Button>Under the hood</Button>
+              <Tooltip title="The learning rate.">
+                <HelpOutlineIcon/>
+              </Tooltip>
             </Grid>
           </Grid>
         </AccordionDetails>
       </Accordion>
+
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Button>Reset defaults</Button>
+        </Grid>
+        <Grid item xs={12}>
+          <Divider/>
+        </Grid>
+        <Grid item xs={12}>
+          <Button>Under the hood</Button>
+        </Grid>
+      </Grid>
     </Paper>
   );
 }
