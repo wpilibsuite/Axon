985e9a34695a99a12fe5a323b1cd8de6e9312074
NLZ432 <NLZ432@gmail.com>
2020-08-21 11:14:12 -0400

    atttempting to download from client

--- packages/client/src/components/project/results/Results.tsx
+++ packages/client/src/components/project/results/Results.tsx
@@ -1,4 +1,16 @@
-import { Container, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@material-ui/core";
+import {
+  Card,
+  CardHeader,
+  CardMedia,
+  Container,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow
+} from "@material-ui/core";
 import React, { ReactElement } from "react";
 import { GetProjectData_project_exports } from "../__generated__/GetProjectData";
 import TestButton from "./TestButton";
@@ -25,6 +37,16 @@ export default function Results(props: { id: string; exports: GetProjectData_pro
           </TableBody>
         </Table>
       </TableContainer>
+
+      <a
+        download="http://localhost:4000/projects/583a0143-2f52-44a3-b249-b088428e8d19/exports/adfsa/tests/asdasd/frame_00000.png"
+        href="http://localhost:4000/projects/583a0143-2f52-44a3-b249-b088428e8d19/exports/adfsa/tests/asdasd/frame_00000.png"
+      >
+        {" "}
+        download{" "}
+      </a>
+
+      <img src="http://localhost:4000/projects/583a0143-2f52-44a3-b249-b088428e8d19/exports/adfsa/tests/asdasd/frame_00000.png" />
     </Container>
   );
 }
