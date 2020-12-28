ce60153c33cedac7282e748f9c081e34eb62c070
GrantPerkins <gcperk20@gmail.com>
2020-12-28 16:05:00 -0500

    Windows start script

--- docker_scripts/windows_start.bat
+++ docker_scripts/windows_start.bat
@@ -1,2 +1,2 @@
-xdg-open http://localhost:3000
+rundll32 url.dll,FileProtocolHandler http://localhost:3000
 docker run -t -i -v /var/run/docker.sock:/var/run/docker.sock -p 3000:3000 -p 4000:4000 gcperkins/axon:latest
