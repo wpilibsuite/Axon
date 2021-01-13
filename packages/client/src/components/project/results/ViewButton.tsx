fbf99605c926d57d10ce0b63839990232e895d7e
Yoni W <yweiner@wpi.edu>
2021-01-13 16:06:22 -0700

    New Tab

--- packages/client/src/components/project/results/ViewButton.tsx
+++ packages/client/src/components/project/results/ViewButton.tsx
@@ -2,37 +2,8 @@ import { Button } from "@material-ui/core";
 import React, { ReactElement } from "react";
 
 export default function StreamViewer(): ReactElement {
-  // const [streamLoading, setStreamLoading] = React.useState(true);
-  // const [tempError, setTempError] = React.useState(false);
-
-  // const handleStreamError = () => {
-  //   setTempError(true);
-  //   setTimeout(() => {
-  //     setTempError(false);
-  //   }, 500);
-  // };
-
-  // const handleStreamLoaded = () => {
-  //   setStreamLoading(false);
-  // };
-
-  // if (streamLoading)
-  //   return (
-  //     <>
-  //       <CircularProgress />
-  //       {!tempError && (
-  //         <img
-  //           src={"http://localhost:5000/stream.mjpg"} //<-- so the images dont cache.
-  //           onError={() => handleStreamError()}
-  //           onLoad={() => handleStreamLoaded()}
-  //           alt="no stream"
-  //         />
-  //       )}
-  //     </>
-  //   );
-  // else
   return (
-    <a href="http://localhost:5000/stream.mjpg">
+    <a href="http://localhost:5000/stream.mjpg" target="_blank">
       <Button variant="outlined" color="secondary">
         View
       </Button>
