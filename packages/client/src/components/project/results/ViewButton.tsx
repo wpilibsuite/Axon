5a7933cae8b7a8596e95312f276b682b1bce3f1e
Yoni W <yweiner@wpi.edu>
2021-01-13 16:13:29 -0700

    No err no problem

--- packages/client/src/components/project/results/ViewButton.tsx
+++ packages/client/src/components/project/results/ViewButton.tsx
@@ -3,7 +3,7 @@ import React, { ReactElement } from "react";
 
 export default function StreamViewer(): ReactElement {
   return (
-    <a href="http://localhost:5000/stream.mjpg" target="_blank">
+    <a href="http://localhost:5000/stream.mjpg" target="_blank" rel="noopener noreferrer">
       <Button variant="outlined" color="secondary">
         View
       </Button>
