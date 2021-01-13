987f87f22c97d1fd5f23b8f3eb28deb5508cfcc6
Nestor <nlzx432@gmail.com>
2021-01-13 17:16:00 -0500

    suppress all the bells and whistles and just put a link to the stream when the test container starts

--- packages/client/src/components/project/results/ViewButton.tsx
+++ packages/client/src/components/project/results/ViewButton.tsx
@@ -1,41 +1,41 @@
-import { Button, CircularProgress } from "@material-ui/core";
+import { Button } from "@material-ui/core";
 import React, { ReactElement } from "react";
 
 export default function StreamViewer(): ReactElement {
-  const [streamLoading, setStreamLoading] = React.useState(true);
-  const [tempError, setTempError] = React.useState(false);
+  // const [streamLoading, setStreamLoading] = React.useState(true);
+  // const [tempError, setTempError] = React.useState(false);
 
-  const handleStreamError = () => {
-    setTempError(true);
-    setTimeout(() => {
-      setTempError(false);
-    }, 500);
-  };
+  // const handleStreamError = () => {
+  //   setTempError(true);
+  //   setTimeout(() => {
+  //     setTempError(false);
+  //   }, 500);
+  // };
 
-  const handleStreamLoaded = () => {
-    setStreamLoading(false);
-  };
+  // const handleStreamLoaded = () => {
+  //   setStreamLoading(false);
+  // };
 
-  if (streamLoading)
-    return (
-      <>
-        <CircularProgress />
-        {!tempError && (
-          <img
-            src={"http://localhost:5000/stream.mjpg"} //<-- so the images dont cache.
-            onError={() => handleStreamError()}
-            onLoad={() => handleStreamLoaded()}
-            alt="no stream"
-          />
-        )}
-      </>
-    );
-  else
-    return (
-      <a href="http://localhost:5000/stream.mjpg">
-        <Button variant="outlined" color="secondary">
-          View
-        </Button>
-      </a>
-    );
+  // if (streamLoading)
+  //   return (
+  //     <>
+  //       <CircularProgress />
+  //       {!tempError && (
+  //         <img
+  //           src={"http://localhost:5000/stream.mjpg"} //<-- so the images dont cache.
+  //           onError={() => handleStreamError()}
+  //           onLoad={() => handleStreamLoaded()}
+  //           alt="no stream"
+  //         />
+  //       )}
+  //     </>
+  //   );
+  // else
+  return (
+    <a href="http://localhost:5000/stream.mjpg">
+      <Button variant="outlined" color="secondary">
+        View
+      </Button>
+    </a>
+  );
 }
