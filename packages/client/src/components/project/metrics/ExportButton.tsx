e05556aa8d0d9aed9dee2a9e9e294a887a2a6e32
Nestor <nlzx432@gmail.com>
2021-01-14 01:44:05 -0500

    new export button takes the place of old one, ExportButton.tsx

--- packages/client/src/components/project/metrics/ExportButton.tsx
+++ packages/client/src/components/project/metrics/ExportButton.tsx
@@ -1,92 +1,47 @@
-import React, { ReactElement } from "react";
-import {
-  Button,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogTitle,
-  IconButton,
-  TextField,
-  Tooltip
-} from "@material-ui/core";
-import gql from "graphql-tag";
-import { useQuery } from "@apollo/client";
+import { GetCheckpoints_project_checkpoints } from "./__generated__/GetCheckpoints";
+import { Tooltip, Button } from "@material-ui/core";
 import { useMutation } from "@apollo/client";
-import { GetProjectData_project_checkpoints } from "../__generated__/GetProjectData";
-import { GET_DOCKER_STATE } from "../../trainerStatus/TrainerStatus";
-import { GetDockerState } from "../../trainerStatus/__generated__/GetDockerState";
-import { DockerState } from "../../../__generated__/globalTypes";
-import { GetExportjobs_exportjobs } from "./__generated__/GetExportjobs";
-import { CircularProgress } from "@material-ui/core";
+import React, { ReactElement } from "react";
+import { gql } from "@apollo/client";
+type Checkpoint = GetCheckpoints_project_checkpoints;
 
-const EXPORT_CHECKPOINT_BUTTON_MUTATION = gql`
-  mutation exportCheckpointButton($id: ID!, $checkpointID: String!, $name: String!) {
+const EXPORT_CHECKPOINT_MUTATION = gql`
+  mutation exportCheckpoint($id: ID!, $checkpointID: String!, $name: String!) {
     exportCheckpoint(id: $id, checkpointID: $checkpointID, name: $name) {
       id
     }
   }
 `;
 
-export default function ExportButton(props: {
-  id: string;
-  checkpoint: GetProjectData_project_checkpoints;
-  job: GetExportjobs_exportjobs | undefined;
-}): ReactElement {
-  const [exportCheckpoint] = useMutation(EXPORT_CHECKPOINT_BUTTON_MUTATION);
-  const [open, setOpen] = React.useState(false);
-  const [name, setName] = React.useState("");
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-  const handleClose = () => {
-    setOpen(false);
-  };
+export function ExportButton(props: { id: string; selected: Checkpoint | null; onExport: () => void }): ReactElement {
+  const [exportCheckpoint] = useMutation(EXPORT_CHECKPOINT_MUTATION);
   const handleExport = () => {
-    exportCheckpoint({
-      variables: {
-        id: props.id,
-        checkpointID: props.checkpoint.id,
-        name
-      }
-    }).catch((err) => {
+    const name = `STEP-${props.selected?.step}-${new Date().getHours()}-${new Date().getMinutes()}`;
+    const checkpointID = props.selected?.id;
+    const id = props.id;
+    exportCheckpoint({ variables: { id, checkpointID, name } }).catch((err) => {
       console.log(err);
     });
-    handleClose();
+    props.onExport();
   };
 
-  const { data, loading, error } = useQuery<GetDockerState>(GET_DOCKER_STATE, { pollInterval: 5000 });
-  if (loading) return <p>connecting to exporter</p>;
-  if (error) return <p>cant connect to exporter</p>;
-  if (data?.dockerState !== DockerState.READY && data?.dockerState !== DockerState.TEST_PULL)
-    return <p>no export image yet</p>;
-  if (props.job !== undefined) return <CircularProgress />;
-
+  if (props.selected === null)
+    return (
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <Tooltip title="Use the graph to select a checkpoint.">
+          <span>
+            <Button variant="outlined" disabled>
+              Export
+            </Button>
+          </span>
+        </Tooltip>
+      </div>
+    );
   return (
-    <>
-      <Tooltip title="Export">
-        <IconButton onClick={handleClickOpen}>Export</IconButton>
-      </Tooltip>
-      <Dialog onClose={handleClose} open={open}>
-        <DialogTitle>Export Checkpoint</DialogTitle>
-        <DialogContent dividers>
-          <TextField
-            onChange={(event) => setName(event.target.value)}
-            autoFocus
-            margin="dense"
-            label="Model Name"
-            fullWidth
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button autoFocus onClick={handleClose}>
-            Cancel
-          </Button>
-          <Button autoFocus onClick={handleExport} color="primary">
-            Export
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </>
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <Button variant="outlined" color="primary" onClick={handleExport}>
+        Export
+      </Button>
+    </div>
   );
 }
--- packages/client/src/components/project/metrics/Metrics.tsx
+++ packages/client/src/components/project/metrics/Metrics.tsx
@@ -1,18 +1,8 @@
-import { Tooltip, Button } from "@material-ui/core";
 import { GetCheckpoints_project_checkpoints } from "./__generated__/GetCheckpoints";
-import { gql } from "@apollo/client";
-import { useMutation } from "@apollo/client";
+import { ExportButton } from "./ExportButton";
 import React, { ReactElement } from "react";
 import Chart from "./Chart";
 type Checkpoint = GetCheckpoints_project_checkpoints;
-
-const EXPORT_CHECKPOINT_MUTATION = gql`
-  mutation exportCheckpoint($id: ID!, $checkpointID: String!, $name: String!) {
-    exportCheckpoint(id: $id, checkpointID: $checkpointID, name: $name) {
-      id
-    }
-  }
-`;
 
 export default function Metrics(props: { id: string }): ReactElement {
   const [selected, setSelected] = React.useState<Checkpoint | null>(null);
@@ -29,38 +19,5 @@ export default function Metrics(props: { id: string }): ReactElement {
       <Chart id={props.id} onClick={onCheckpoint} selected={selected} />
       <ExportButton id={props.id} selected={selected} onExport={onExport} />
     </>
-  );
-}
-
-export function ExportButton(props: { id: string; selected: Checkpoint | null; onExport: () => void }): ReactElement {
-  const [exportCheckpoint] = useMutation(EXPORT_CHECKPOINT_MUTATION);
-  const handleExport = () => {
-    const name = `STEP-${props.selected?.step}-${new Date().getHours()}-${new Date().getMinutes()}`;
-    const checkpointID = props.selected?.id;
-    const id = props.id;
-    exportCheckpoint({ variables: { id, checkpointID, name } }).catch((err) => {
-      console.log(err);
-    });
-    props.onExport();
-  };
-
-  if (props.selected === null)
-    return (
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <Tooltip title="Use the graph to select a checkpoint.">
-          <span>
-            <Button variant="outlined" disabled>
-              Export
-            </Button>
-          </span>
-        </Tooltip>
-      </div>
-    );
-  return (
-    <div style={{ display: "flex", justifyContent: "center" }}>
-      <Button variant="outlined" color="primary" onClick={handleExport}>
-        Export
-      </Button>
-    </div>
   );
 }
