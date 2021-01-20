cc2a38dea0ae574b5d72c04cada6d7696c0c0609
GabrielDeml <gabrielddeml@gmail.com>
2021-01-20 06:27:45 -0800

    Things seem to somewhat work

--- containers/utils/detect_image.py
+++ containers/utils/detect_image.py
@@ -23,96 +23,117 @@ from PIL import ImageDraw
 import detect
 import tflite_runtime.interpreter as tflite
 import platform
+from cscore import CameraServer, VideoSource, UsbCamera, MjpegServer
+from networktables import NetworkTablesInstance
+import numpy as np
 
+# TODO Make me right
+team = 1519
 
 
 def load_labels(path, encoding='utf-8'):
-  """Loads labels from file (with or without index numbers).
+    """Loads labels from file (with or without index numbers).
 
-  Args:
-    path: path to label file.
-    encoding: label file encoding.
-  Returns:
-    Dictionary mapping indices to labels.
-  """
-  with open(path, 'r', encoding=encoding) as f:
-    lines = f.readlines()
-    if not lines:
-      return {}
+    Args:
+      path: path to label file.
+      encoding: label file encoding.
+    Returns:
+      Dictionary mapping indices to labels.
+    """
+    with open(path, 'r', encoding=encoding) as f:
+        lines = f.readlines()
+        if not lines:
+            return {}
 
-    if lines[0].split(' ', maxsplit=1)[0].isdigit():
-      pairs = [line.split(' ', maxsplit=1) for line in lines]
-      return {int(index): label.strip() for index, label in pairs}
-    else:
-      return {index: line.strip() for index, line in enumerate(lines)}
+        if lines[0].split(' ', maxsplit=1)[0].isdigit():
+            pairs = [line.split(' ', maxsplit=1) for line in lines]
+            return {int(index): label.strip() for index, label in pairs}
+        else:
+            return {index: line.strip() for index, line in enumerate(lines)}
 
 
 def make_interpreter(model_file):
-  model_file, *device = model_file.split('@')
-  return tflite.Interpreter(
-      model_path=model_file,
-      experimental_delegates=[
-          tflite.load_delegate("libedgetpu.so.1",
-                               {'device': device[0]} if device else {})
-      ])
+    model_file, *device = model_file.split('@')
+    return tflite.Interpreter(
+        model_path=model_file,
+        experimental_delegates=[
+            tflite.load_delegate("libedgetpu.so.1",
+                                 {'device': device[0]} if device else {})
+        ])
 
 
 def draw_objects(draw, objs, labels):
-  """Draws the bounding box and label for each object."""
-  for obj in objs:
-    bbox = obj.bbox
-    draw.rectangle([(bbox.xmin, bbox.ymin), (bbox.xmax, bbox.ymax)],
-                   outline='red')
-    draw.text((bbox.xmin + 10, bbox.ymin + 10),
-              '%s\n%.2f' % (labels.get(obj.id, obj.id), obj.score),
-              fill='red')
+    """Draws the bounding box and label for each object."""
+    for obj in objs:
+        bbox = obj.bbox
+        draw.rectangle([(bbox.xmin, bbox.ymin), (bbox.xmax, bbox.ymax)],
+                       outline='red')
+        draw.text((bbox.xmin + 10, bbox.ymin + 10),
+                  '%s\n%.2f' % (labels.get(obj.id, obj.id), obj.score),
+                  fill='red')
 
 
 def main():
-  parser = argparse.ArgumentParser(
-      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-  parser.add_argument('-m', '--model', required=True,
-                      help='File path of .tflite file.')
-  parser.add_argument('-i', '--input', required=True,
-                      help='File path of image to process.')
-  parser.add_argument('-l', '--labels',
-                      help='File path of labels file.')
-  parser.add_argument('-t', '--threshold', type=float, default=0.4,
-                      help='Score threshold for detected objects.')
-  parser.add_argument('-o', '--output',
-                      help='File path for the result image with annotations')
-  args = parser.parse_args()
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-m', '--model', required=True,
+                        help='File path of .tflite file.')
+    parser.add_argument('-i', '--input', required=True,
+                        help='File path of image to process.')
+    parser.add_argument('-l', '--labels',
+                        help='File path of labels file.')
+    parser.add_argument('-t', '--threshold', type=float, default=0.4,
+                        help='Score threshold for detected objects.')
+    parser.add_argument('-o', '--output',
+                        help='File path for the result image with annotations')
+    parser.add_argument('-n', '--team', type=int,
+                        help='Team number')
+    args = parser.parse_args()
 
-  labels = load_labels(args.labels) if args.labels else {}
-  interpreter = make_interpreter(args.model)
-  interpreter.allocate_tensors()
+    # Init cameras
+    WIDTH, HEIGHT = 720, 1920
+    print("Connecting to Network Tables")
+    ntinst = NetworkTablesInstance.getDefault()
+    ntinst.startClientTeam(args.team)
+    # """Format of these entries found in WPILib documentation."""
+    # detections_entry = ntinst.getTable("ML").getEntry("detections")
+    # print("Starting camera server")
+    cs = CameraServer.getInstance()
+    camera = cs.startAutomaticCapture()
+    camera.setResolution(WIDTH, HEIGHT)
+    cv_sink = cs.getVideo()
+    img = np.zeros(shape=(HEIGHT, WIDTH, 3), dtype=np.uint8)
+    # output = cs.putVideo("MLOut", WIDTH, HEIGHT)
 
-  image = Image.open(args.input)
-  scale = detect.set_input(interpreter, image.size,
-                           lambda size: image.resize(size, Image.ANTIALIAS))
+    labels = load_labels(args.labels) if args.labels else {}
+    interpreter = make_interpreter(args.model)
+    interpreter.allocate_tensors()
+    while True:
+        t, frame = cv_sink.grabFrame(img)
+        image = Image.fromarray(frame)
+        scale = detect.set_input(interpreter, image.size,
+                                 lambda size: image.resize(size, Image.ANTIALIAS))
+        interpreter.invoke()
+        objs = detect.get_output(interpreter, args.threshold, scale)
 
-  print('----INFERENCE TIME----')
-  print('Note: The first inference is slow because it includes',
-        'loading the model into Edge TPU memory.')
-  interpreter.invoke()
-  objs = detect.get_output(interpreter, args.threshold, scale)
+        print('-------RESULTS--------')
+        if not objs:
+            print('No objects detected')
 
-  print('-------RESULTS--------')
-  if not objs:
-    print('No objects detected')
+        for obj in objs:
+            print(labels.get(obj.id, obj.id))
+            print('  id:    ', obj.id)
+            print('  score: ', obj.score)
+            print('  bbox:  ', obj.bbox)
 
-  for obj in objs:
-    print(labels.get(obj.id, obj.id))
-    print('  id:    ', obj.id)
-    print('  score: ', obj.score)
-    print('  bbox:  ', obj.bbox)
+    if args.output:
+        image = image.convert('RGB')
+        draw_objects(ImageDraw.Draw(image), objs, labels)
+        image.save(args.output)
+        image.show()
 
-  if args.output:
-    image = image.convert('RGB')
-    draw_objects(ImageDraw.Draw(image), objs, labels)
-    image.save(args.output)
-    image.show()
+    # cs.removeServer(cs.name)
 
 
 if __name__ == '__main__':
-  main()
+    main()
