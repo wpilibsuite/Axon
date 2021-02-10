61df93a1f1846fc4c47fe0f85d802971d8ff3ceb
GrantPerkins <gcperk20@gmail.com>
2021-02-10 17:03:55 -0500

    Create icon

--- packages/launcher/electron/main.ts
+++ packages/launcher/electron/main.ts
@@ -1,60 +1,63 @@
-import { app, BrowserWindow } from "electron";
+import {app, BrowserWindow, nativeImage} from "electron";
 import * as path from "path";
 import * as isDev from "electron-is-dev";
-import installExtension, { REACT_DEVELOPER_TOOLS } from "electron-devtools-installer";
+import installExtension, {REACT_DEVELOPER_TOOLS} from "electron-devtools-installer";
 
 let win: BrowserWindow | null = null;
 
 function createWindow() {
-  win = new BrowserWindow({
-    width: 600,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  });
+    const icoPath = __dirname + "/icon.png";
+    win = new BrowserWindow({
+        width: 600,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
 
-  if (isDev) {
-    win.loadURL("http://localhost:2000/index.html");
-  } else {
-    // 'build/index.html'
-    win.loadURL(`file://${__dirname}/../index.html`);
-  }
-
-  win.on("closed", () => (win = null));
-
-  win.setMenu(null);
-
-  // Hot Reloading
-  if (isDev) {
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    require("electron-reload")(__dirname, {
-      electron: path.join(__dirname, "..", "..", "node_modules", ".bin", "electron"),
-      forceHardReset: true,
-      hardResetMethod: "exit"
+        },
+        icon: nativeImage.createFromPath(icoPath)
     });
-  }
 
-  // DevTools
-  installExtension(REACT_DEVELOPER_TOOLS)
-    .then((name) => console.log(`Added Extension:  ${name}`))
-    .catch((err) => console.log("An error occurred: ", err));
+    if (isDev) {
+        win.loadURL("http://localhost:2000/index.html");
+    } else {
+        // 'build/index.html'
+        win.loadURL(`file://${__dirname}/../index.html`);
+    }
 
-  if (isDev) {
-    win.webContents.openDevTools();
-  }
+    win.on("closed", () => (win = null));
+
+    win.setMenu(null);
+
+    // Hot Reloading
+    if (isDev) {
+        // eslint-disable-next-line @typescript-eslint/no-var-requires
+        require("electron-reload")(__dirname, {
+            electron: path.join(__dirname, "..", "..", "node_modules", ".bin", "electron"),
+            forceHardReset: true,
+            hardResetMethod: "exit"
+        });
+    }
+
+    // DevTools
+    installExtension(REACT_DEVELOPER_TOOLS)
+        .then((name) => console.log(`Added Extension:  ${name}`))
+        .catch((err) => console.log("An error occurred: ", err));
+
+    if (isDev) {
+        win.webContents.openDevTools();
+    }
 }
 
 app.on("ready", createWindow);
 
 app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
 });
 
 app.on("activate", () => {
-  if (win === null) {
-    createWindow();
-  }
+    if (win === null) {
+        createWindow();
+    }
 });
