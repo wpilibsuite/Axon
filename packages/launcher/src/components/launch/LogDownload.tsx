a804601eb3e22667de747daf4b4393cf29fdcb55
GrantPerkins <gcperk20@gmail.com>
2021-02-10 20:22:02 -0500

    Format

--- packages/launcher/src/components/launch/LogDownload.tsx
+++ packages/launcher/src/components/launch/LogDownload.tsx
@@ -1,8 +1,8 @@
-import React, {ReactElement} from "react";
-import {makeStyles} from "@material-ui/core/styles";
-import {Button, Container, Grid, IconButton, ListItem, Tooltip, Typography} from "@material-ui/core";
+import React, { ReactElement } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Button, Container, Grid, IconButton, ListItem, Tooltip, Typography } from "@material-ui/core";
 import path from "path";
-import {GetApp} from "@material-ui/icons";
+import { GetApp } from "@material-ui/icons";
 
 const fs = window.require("fs");
 const browser = window.require("browser");
@@ -11,51 +11,48 @@ const browser = window.require("browser");
 const Dockerode2 = window.require("dockerode"); // used for connecting to docker socket
 
 const useStyles = makeStyles((theme) => ({
-    logo: {
-        height: 200,
-        width: 180
-    },
-    start: {
-        height: 50,
-        width: 50
-    }
+  logo: {
+    height: 200,
+    width: 180
+  },
+  start: {
+    height: 50,
+    width: 50
+  }
 }));
 
-
 export default function LogDownload(): ReactElement {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    const logFilePath = window.require("electron-log").transports.file.getFile().path;
+  const logFilePath = window.require("electron-log").transports.file.getFile().path;
 
+  const onClick = () => {
+    fs.readFile(logFilePath, "utf-8", (err: NodeJS.ErrnoException | null, data: string) => {
+      if (err) {
+        alert("An error ocurred reading the file :" + err.message);
+        return;
+      }
+      if (!data) data = "Axon not run.";
+      fs.writeFile("./public/axon.log", data, function (err: NodeJS.ErrnoException | null) {
+        if (err) throw err;
+        console.log("Saved!");
+      });
+    });
+  };
 
-    const onClick = () => {
-        fs.readFile(logFilePath, 'utf-8', (err: NodeJS.ErrnoException | null, data: string) => {
-            if (err) {
-                alert("An error ocurred reading the file :" + err.message);
-                return;
-            }
-            if (!data) data = "Axon not run.";
-            fs.writeFile("./public/axon.log", data, function (err: NodeJS.ErrnoException | null) {
-                if (err) throw err;
-                console.log('Saved!');
-            });
-        });
-
-    };
-
-    return (
-        <a
-            href={`http://localhost:2000/axon.log`}
-            style={{
-                color: "inherit",
-                textDecoration: "none"
-            }}
-            onClick={onClick}
-            download
-        >
-            <IconButton>
-                <GetApp className={classes.start}/>
-            </IconButton>
-        </a>
-    );
+  return (
+    <a
+      href={`http://localhost:2000/axon.log`}
+      style={{
+        color: "inherit",
+        textDecoration: "none"
+      }}
+      onClick={onClick}
+      download
+    >
+      <IconButton>
+        <GetApp className={classes.start} />
+      </IconButton>
+    </a>
+  );
 }
--- packages/launcher/src/docker/Localhost.ts
+++ packages/launcher/src/docker/Localhost.ts
@@ -1,16 +1,12 @@
-
 const waitOn = window.require("wait-on");
 export default class Localhost {
   async waitForStart(): Promise<void> {
+    await waitOn({
+      timeout: 10000,
+      resources: ["http://localhost:3000"]
+    });
 
-    await waitOn(
-        {
-          timeout: 10000,
-          resources: ["http://localhost:3000"]
-        }
-    )
-
-    const {shell} = window.require("electron");
+    const { shell } = window.require("electron");
     shell.openExternal("http://localhost:3000");
   }
 }
