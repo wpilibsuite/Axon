64e3e7e5e8dda3828bfbb2cf274bb65b8cd92ca5
GrantPerkins <gcperk20@gmail.com>
2021-02-11 16:04:44 -0500

    Flip warning logic

--- packages/launcher/src/components/launch/Launch.tsx
+++ packages/launcher/src/components/launch/Launch.tsx
@@ -83,7 +83,7 @@ export default function Launch(): ReactElement {
     }
   };
   docker.isConnected().then((value) => {
-    setOpen(value && !clicked);
+    setOpen(!value && !clicked);
   });
 
   return (
