374e55818b7f7f65e1e78c03a341cd3c1e8358f6
GrantPerkins <gcperk20@gmail.com>
2021-05-25 15:10:58 -0400

    Fix OpenImages download

--- containers/utils/openimages/download.py
+++ containers/utils/openimages/download.py
@@ -2,18 +2,29 @@ import json
 import sys
 from glob import glob
 import os
+from os.path import basename
 from shutil import copyfile
 import cv2
 import pandas
 from openimages.download import download_dataset
+from zipfile import ZipFile
 
 
 class OpenImagesDownloader:
+    """
+    Class for converting OpenImages format into an Axon usable format (TFRecords)
+    """
+
     def __init__(self, data_json):
+        """
+        Set up attributes, parse JSON
+        :param data_json:
+        """
         assert os.path.isfile(data_json)
         with open(data_json) as file:
             self.data = json.load(file)
         self.labels = self.data["labels"]
+        print(self.labels)
         assert type(self.labels) == list
         self.limit = self.data["limit"]
         assert type(self.limit) == int
@@ -25,10 +36,23 @@ class OpenImagesDownloader:
         self.images = glob(self.directory + "/*/images/*.jpg")
 
     def download(self):
+        """
+        API call to download OpenImages slice
+        :return: None
+        """
         download_dataset(dest_dir=self.directory, csv_dir=self.directory, class_labels=self.labels,
                          annotation_format="pascal", exclusions_path=None, limit=self.limit)
 
     def parse_line(self, key, label, height, width, box):
+        """
+        Add a line to the csv
+        :param key:
+        :param label:
+        :param height:
+        :param width:
+        :param box:
+        :return:
+        """
         self.csv.append({
             "filename": key,
             "height": height,
@@ -41,6 +65,7 @@ class OpenImagesDownloader:
         })
 
     def create_csv(self):
+        print("Parsing huge .csv. Give me a minute.")
         self.images = glob(self.directory + "/*/images/*.jpg")
         with open(os.path.join(self.directory, "class-descriptions-boxable.csv")) as file:
             for row in file.readlines():
@@ -73,6 +98,7 @@ class OpenImagesDownloader:
         all_labels_df = pandas.read_csv(os.path.join(self.directory, "train-annotations-bbox.csv"))
         all_labels_df.set_index("ImageID", inplace=True)
 
+        labels = [i.lower() for i in self.labels]
         for key in self.image_data.keys():
             entry = all_labels_df.loc[key]
             height = self.image_data[key]["height"]
@@ -80,17 +106,23 @@ class OpenImagesDownloader:
             if type(entry) == pandas.DataFrame:
                 for row in entry.iterrows():
                     box = {i.lower(): row[1][i] for i in ["XMin", "XMax", "YMin", "YMax"]}
-                    label = self.label_map[row[1]["LabelName"]]
-                    self.parse_line(key+'.jpg', label, height, width, box)
+                    label = self.label_map[row[1]["LabelName"]].lower()
+                    if label in labels:
+                        print(label)
+                        self.parse_line(key + '.jpg', label, height, width, box)
             else:
                 box = {i.lower(): entry.get(i) for i in ["XMin", "XMax", "YMin", "YMax"]}
-                label = self.label_map[entry.get("LabelName")]
-                self.parse_line(key+'.jpg', label, height, width, box)
+                label = self.label_map[entry.get("LabelName")].lower()
+                if label in labels:
+                    print(label)
+                    self.parse_line(key + '.jpg', label, height, width, box)
 
+        print("creating subfolders")
         self.create_subfolders("train")
         self.create_subfolders("test")
 
     def create_subfolders(self, name):
+
         with open("tar/{}/_annotations.csv".format(name), 'w+') as csv:
             csv.write("filename,width,height,class,xmin,ymin,xmax,ymax\n")
             r = range(len(self.csv))[:int(len(self.csv) * .7)] if name == "train" else range(len(self.csv))[
@@ -99,31 +131,26 @@ class OpenImagesDownloader:
                 row = self.csv[i]
                 csv.write("{},{},{},{},{},{},{},{}\n".format(row["filename"], row["width"], row["height"], row["class"],
                                                              row["xmin"], row["ymin"], row["xmax"], row["ymax"]))
-                copyfile("tar/" + row["filename"], "tar/" + name + '/' + row["filename"] + ".jpg")
-                self.label_frame("tar/" + row["filename"], row["class"], row["xmin"], row["xmax"], row["ymin"],
-                                 row["ymax"])
+                copyfile("tar/" + row["filename"], "tar/" + name + '/' + row["filename"])
+                # self.label_frame("tar/" + row["filename"], row["class"], row["xmin"], row["xmax"], row["ymin"], row["ymax"])
 
-    def label_frame(self, filename, label, xmin, xmax, ymin, ymax):
-        score = float(1)
-        print(filename, label)
-        frame = cv2.imread(filename)
-
-        cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (10, 255, 0), 4)
-
-        # Draw label
-        # Look up object name from "labels" array using class index
-        label = '%s: %d%%' % (label, score * 100)  # Example: 'person: 72%'
-        label_size, base = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2)  # Get font size
-        label_ymin = max(ymin, label_size[1] + 10)  # Make sure not to draw label too close to top of window
-        cv2.rectangle(frame, (xmin, label_ymin - label_size[1] - 10), (xmin + label_size[0], label_ymin + base - 10),
-                      (255, 255, 255), cv2.FILLED)
-        # Draw label text
-        cv2.putText(frame, label, (xmin, label_ymin - 7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2)
-        cv2.imwrite("out/" + filename.split('/')[-1], frame)
+    def make_zip(self):
+        with ZipFile("dataset.zip", 'w') as zipFile:
+            for directory in "train test".split():
+                for folderName, subfolders, filenames in os.walk("tar/" + directory):
+                    for filename in filenames:
+                        print(filename)
+                        # create complete filepath of file in directory
+                        file = os.path.join(directory, filename)
+                        # Add file to zip
+                        zipFile.write("tar/" + directory + '/' + filename, file)
+        print("done.")
 
 
 if __name__ == "__main__":
     data_json = sys.argv[1]
     downloader = OpenImagesDownloader(data_json)
     # downloader.download()
-    downloader.create_csv()
+    # downloader.create_csv()
+    print("Making archive")
+    downloader.make_zip()
