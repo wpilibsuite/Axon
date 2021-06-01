e33abf77804bd75b7a41c0727e3d1d1500e9f92b
GrantPerkins <gcperk20@gmail.com>
2021-06-01 10:06:53 -0400

    State changes working

--- packages/client/src/components/navigationDrawer/CreateDatasetDialogButton.tsx
+++ packages/client/src/components/navigationDrawer/CreateDatasetDialogButton.tsx
@@ -14,7 +14,7 @@ import {
 import { useApolloClient } from "@apollo/client";
 import { TreeItem } from "@material-ui/lab";
 import { makeStyles } from "@material-ui/core/styles";
-import { Add, ControlPoint, Create } from "@material-ui/icons";
+import { Add, ControlPoint, Create, RemoveCircleOutline } from "@material-ui/icons";
 
 const useStyles = makeStyles((theme) => ({
   item: {
@@ -58,9 +58,18 @@ export default function CreateDatasetDialogButton(): ReactElement {
     setOpen(false);
   };
   const append = () => {
-    // keys.push("");
-    console.log("append");
-    setKeys(keys.concat([""]));
+    const test: string[] = [...keys];
+    setKeys(test.concat([" "]));
+  };
+  const update = (index: number, element: string) => {
+    const test: string[] = [...keys];
+    test[index] = element;
+    setKeys(test);
+  };
+  const remove = (index: number) => {
+    const test: string[] = [...keys];
+    test.splice(index, 1);
+    setKeys(test);
   };
 
   return (
@@ -68,16 +77,32 @@ export default function CreateDatasetDialogButton(): ReactElement {
       <Dialog open={open}>
         <DialogTitle>Create Dataset from OpenImages</DialogTitle>
         <DialogContent dividers>
-          {keys.map((obj: string) => {
-            return (
-              <div key={obj}>
-                <TextField key={obj} placeholder={obj} />
-              </div>
-            );
-          })}
-          <IconButton style={{ justifyContent: "center" }} onClick={append}>
-            <ControlPoint />
-          </IconButton>
+          <Grid container alignItems="center" justify="center" style={{ maxWidth: "400px" }}>
+            {keys.map((obj: string, index: number) => {
+              return (
+                <>
+                  <Grid item xs={2} key={index}>
+                    <IconButton onClick={() => remove(index)}>
+                      <RemoveCircleOutline />
+                    </IconButton>
+                  </Grid>
+                  <Grid item xs={10}>
+                    <TextField
+                      key={index}
+                      placeholder={obj}
+                      onChange={(event) => update(index, event.target.value)}
+                      style={{ width: "100%" }}
+                    />
+                  </Grid>
+                </>
+              );
+            })}
+            <Grid item xs={12}>
+              <IconButton style={{ justifyContent: "center" }} onClick={append}>
+                <ControlPoint />
+              </IconButton>
+            </Grid>
+          </Grid>
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose}>Close</Button>
