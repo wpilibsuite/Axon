c2c10710a47fcee791679dd09a27aa7ca224596f
Alexander Sun <technoal9@gmail.com>
2021-06-09 10:26:54 -0400

    added text wrapping for project and dataset names

--- packages/client/src/components/navigationDrawer/NavigationTreeItem.tsx
+++ packages/client/src/components/navigationDrawer/NavigationTreeItem.tsx
@@ -49,9 +49,13 @@ export default function NavigationTreeItem({ text, nodeId, icon }: ProjectListPr
       label={
         <div className={classes.labelRoot}>
           {React.createElement(icon, { className: classes.labelIcon })}
+<<<<<<< HEAD
           <Typography variant={"body1"} className={classes.dataset}>
             {text}
           </Typography>
+=======
+          <Typography variant={"body1"} className={classes.dataset}>{text}</Typography>
+>>>>>>> added text wrapping for project and dataset names
         </div>
       }
     />
