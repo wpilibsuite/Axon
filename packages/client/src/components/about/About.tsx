5c8f935c178fd674e8f4fededb4641d2588e199d
Grant Perkins <24625614+GrantPerkins@users.noreply.github.com>
2021-06-10 11:49:15 -0400

    About page link to docs (#208)
    * New about page
    
    * Format

--- packages/client/src/components/about/About.tsx
+++ packages/client/src/components/about/About.tsx
@@ -2,7 +2,13 @@ import React, { ReactElement } from "react";
 
 export default function About(): ReactElement {
   return (
-    <iframe height="100%" title="docs" width="100%" src="https://docs.wpilib.org/" allowFullScreen={true}>
+    <iframe
+      height="100%"
+      title="docs"
+      width="100%"
+      src="https://docs.wpilib.org/en/latest/docs/software/wpilib-tools/axon/introduction.html"
+      allowFullScreen={true}
+    >
       Docs
     </iframe>
   );
