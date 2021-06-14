653593c2ad2abe53dfd66aa0126fb7858321036c
GrantPerkins <gcperk20@gmail.com>
2021-06-14 09:26:50 -0400

    Remove TrainStatus from Creator

--- packages/server/src/mL/Creator.ts
+++ packages/server/src/mL/Creator.ts
@@ -15,14 +15,11 @@ export default class Creator {
 
   readonly classes: string[];
   readonly maxImages: number;
-
-  private status: TrainStatus;
   readonly docker: Docker;
   readonly directory: string;
   readonly id: string;
 
   public constructor(docker: Docker, classes: string[], maxImages: number, id: string) {
-    this.status = TrainStatus.Idle;
     this.docker = docker;
     this.classes = classes;
     this.maxImages = maxImages;
@@ -34,8 +31,6 @@ export default class Creator {
    * Create the training parameter file in the container's mounted directory to control the container.
    */
   public async writeParameterFile(): Promise<void> {
-    this.status = TrainStatus.Writing;
-
     const createParameters: CreateParameters = {
       labels: this.classes,
       limit: this.maxImages
@@ -50,11 +45,7 @@ export default class Creator {
    * Starts training. Needs to have the dataset record and hyperparameters.json in the working directory.
    */
   public async createDataset(): Promise<void> {
-    this.status = TrainStatus.Training;
-
     const container = await this.docker.createContainerProjectless(`${this.id}`, this.id, Creator.images.openimages);
-
     await this.docker.runContainer(container);
-    this.status = TrainStatus.Stopped;
   }
 }
