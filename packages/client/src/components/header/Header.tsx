a30082629698b0b116f724e1cea5884fa8c188e4
Grant Perkins <24625614+GrantPerkins@users.noreply.github.com>
2021-06-19 18:08:03 -0400

    Show version in header and footer (#212)

--- packages/client/src/components/header/Header.tsx
+++ packages/client/src/components/header/Header.tsx
@@ -2,6 +2,9 @@ import * as React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { AppBar, Toolbar, Typography } from "@material-ui/core";
 import { ReactElement } from "react";
+import gql from "graphql-tag";
+import { useQuery } from "@apollo/client";
+import { GetAxonVersionHeader } from "./__generated__/GetAxonVersionHeader";
 
 const useStyles = makeStyles((theme) => ({
   appBar: {
@@ -9,14 +12,26 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
+const GET_AXON_VERSION_QUERY = gql`
+  query GetAxonVersionHeader {
+    getAxonVersion
+  }
+`;
 export default function Header(): ReactElement {
   const classes = useStyles();
+  const versionQuery = useQuery<GetAxonVersionHeader, GetAxonVersionHeader>(GET_AXON_VERSION_QUERY);
+  const getVersion = () => {
+    if (versionQuery.loading || !versionQuery.data) {
+      return "Version Loading";
+    }
+    return versionQuery.data.getAxonVersion;
+  };
 
   return (
     <AppBar position="fixed" className={classes.appBar}>
       <Toolbar>
         <Typography variant="h6" color="inherit" noWrap>
-          Axon
+          Axon: {getVersion()}
         </Typography>
       </Toolbar>
     </AppBar>
