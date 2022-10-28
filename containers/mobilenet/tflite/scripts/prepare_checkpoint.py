ec2dbae8c6b05df036f6b57c249520ac70fe9ea6
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-28 17:40:33 +0000

    Adding tarfile member sanitization to extractall()

--- containers/mobilenet/tflite/scripts/prepare_checkpoint.py
+++ containers/mobilenet/tflite/scripts/prepare_checkpoint.py
@@ -12,4 +12,26 @@ def prepare_checkpoint(checkpoint_tar):
     model_name = "/opt/ml/model/ssd_mobilenet_v2_quantized_300x300_coco_2019_01_03" + '.tar.gz'
 
     with tarfile.open(model_name) as model:
-        model.extractall(CKPT_DIR)
+        
+        import os
+        
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(model, CKPT_DIR)
